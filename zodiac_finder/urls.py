951ec5b87104641bb03cbbe8c65b15be95e9e13b
Amir22we <atazanovamir19@gmail.com>
2025-12-08 15:45:29 +0500

    Added About Site page

--- zodiac_finder/urls.py
+++ zodiac_finder/urls.py
@@ -4,5 +4,7 @@ from zodiac_finder import views
 app_name = 'zodiac_finder'
 
 urlpatterns = [
-    path('about_me/', views.about_me, name='about_me')
+    path('about_me/', views.about_me, name='about_me'),
+    path('about_site/', views.about_site, name='about_site') 
 ]
+
--- zodiac_finder/views.py
+++ zodiac_finder/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def about_me(request):
     return render(request, 'zodiac_finder/about_me.html')
+
+def about_site(request):
+    return render(request, 'zodiac_finder/about_site.html')
